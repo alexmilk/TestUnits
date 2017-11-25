b2cbaf63725e4949397ace98a3ab07398926efde
Alex <alex.milkis@kaltura.com>
2017-11-25 19:49:07 +0200

    Added advocate

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function greet(name = "Joe Doe") {
+	console.log("Hello " + name);
+};
+
+greet("Alechko");
+
+
+select id,name,created_at from entry where partner_id=1656501 AND created_at between 2017-08-01 AND 2017-08-05;
+0_01p1dkhj	20150216_111955	2015-04-16 05:29:40
