d1334dbebac8a167e973003724d0c88ecf63714a
Alex <alex.milkis@kaltura.com>
2018-04-01 15:03:27 +0300

    Added custom scripts

---
+++ Scripts/addentry.sh
@@ -0,0 +1,26 @@
+curl -H 'Host: www.kaltura.com' -H 'Cache-Control: max-age=0' -H 'Origin: http://www.kaltura.com' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36' -H 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryUaqY1dSSREPXTRBy' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Referer: http://www.kaltura.com/api_v3/testme/' -H 'Accept-Language: en-US,en;q=0.8' -H 'Cookie: _mkto_trk=id:984-SDM-859&token:_mch-kaltura.com-1495444274060-79610; __utma=78634126.1621856910.1495444274.1495444274.1495444274.1; __utmz=78634126.1495444274.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __cfduid=d09647d369cde0b2b684eb9829db9ace91495459166; trwv.uid=kaltura-1495484859364-50a15c3d%3A1; kalsysauth=718eb7fe4370723fe9d991e9cfb39b505717166e' --data-binary '------WebKitFormBoundaryUaqY1dSSREPXTRBy
+Content-Disposition: form-data; name="format"
+
+2
+------WebKitFormBoundaryUaqY1dSSREPXTRBy
+Content-Disposition: form-data; name="ks"
+
+'$1'
+------WebKitFormBoundaryUaqY1dSSREPXTRBy
+Content-Disposition: form-data; name="service"
+
+baseentry
+------WebKitFormBoundaryUaqY1dSSREPXTRBy
+Content-Disposition: form-data; name="action"
+
+add
+------WebKitFormBoundaryUaqY1dSSREPXTRBy
+Content-Disposition: form-data; name="entry:objectType"
+
+KalturaBaseEntry
+------WebKitFormBoundaryUaqY1dSSREPXTRBy
+Content-Disposition: form-data; name="entry:name"
+
+'$2'
+------WebKitFormBoundaryUaqY1dSSREPXTRBy--
+' --compressed 'http://www.kaltura.com/api_v3/?format=2'
---
+++ Scripts/newks.sh
@@ -0,0 +1,30 @@
+curl -H 'Host: www.kaltura.com' -H 'Cache-Control: max-age=0' -H 'Origin: http://www.kaltura.com' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36' -H 'Content-Type: multipart/form-data; boundary=----WebKitFormBoundaryJ9AYyXiGlLpXudZ0' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Referer: http://www.kaltura.com/api_v3/testme/' -H 'Accept-Language: en-US,en;q=0.8' -H 'Cookie: _mkto_trk=id:984-SDM-859&token:_mch-kaltura.com-1495444274060-79610; __utma=78634126.1621856910.1495444274.1495444274.1495444274.1; __utmz=78634126.1495444274.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __cfduid=d09647d369cde0b2b684eb9829db9ace91495459166; trwv.uid=kaltura-1495484859364-50a15c3d%3A1; kalsysauth=718eb7fe4370723fe9d991e9cfb39b505717166e' --data-binary '------WebKitFormBoundaryJ9AYyXiGlLpXudZ0
+Content-Disposition: form-data; name="format"
+
+1
+------WebKitFormBoundaryJ9AYyXiGlLpXudZ0
+Content-Disposition: form-data; name="service"
+
+session
+------WebKitFormBoundaryJ9AYyXiGlLpXudZ0
+Content-Disposition: form-data; name="action"
+
+start
+------WebKitFormBoundaryJ9AYyXiGlLpXudZ0
+Content-Disposition: form-data; name="secret"
+
+dc944c90997c5217f52422332adc3c3d
+------WebKitFormBoundaryJ9AYyXiGlLpXudZ0
+Content-Disposition: form-data; name="userId"
+
+alex.milkis@kaltura.com
+------WebKitFormBoundaryJ9AYyXiGlLpXudZ0
+Content-Disposition: form-data; name="type"
+
+2
+------WebKitFormBoundaryJ9AYyXiGlLpXudZ0
+Content-Disposition: form-data; name="partnerId"
+
+1666321
+------WebKitFormBoundaryJ9AYyXiGlLpXudZ0--
+' --compressed 'http://www.kaltura.com/api_v3/?format=1'
